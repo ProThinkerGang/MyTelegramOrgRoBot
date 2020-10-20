8b963708185e062d5d1670477c89feff77a6c92f
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-10-20 16:07:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@
 import logging
 import os
 
-from telegram import ParseMode
+from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import (
     Updater,
     CommandHandler,
