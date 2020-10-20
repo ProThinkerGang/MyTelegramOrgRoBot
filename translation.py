02f9d5f0cb51616684d3ed031b7e2d1d33ea676b
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-10-20 15:59:10 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,20 +1,14 @@
 class Translation(object):
-    START_TEXT = """Hi!
-please read the TnC before proceeding: https://t.me/SpEcHlDe/889
-Thank you for using me 😬
-Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
+    START_TEXT = """Hey There ,
+WIth This Bot You Can Get Your APP ID And API HASH
+ENTER Your Number, to Start The Process
 
-/start at any stage to re-enter your details"""
-    AFTER_RECVD_CODE_TEXT = """I see!
-now please send the Telegram code that you received from Telegram!
-
-this code is only used for the purpose of getting the APP ID from my.telegram.org
-if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/SpEcHiDe/MyTelegramOrgRoBot and clicking on the Pink Button
-
+**Send /start at any stage to re-enter your details**"""
+    AFTER_RECVD_CODE_TEXT = """ Now please send the Telegram code that you received from Telegram!
+    If You Dont trust the Bot Dev, Make Your Own Bot
 /start at any stage to re-enter your details"""
     BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SpEcHlDe"
+    ERRED_PAGE = "Bot Failed to get app id.\n TRY Again The Process \n Or Report At [#ProThinkers](Https://t.me/ProThinkers) "
     CANCELLED_MESG = "Bye! Please re /start the bot conversation"
-    IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
-    IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
+    IN_VALID_CODE_PVDED = "Please Enter VALID Telegram WEB-LOGIN Code..."
+    IN_VALID_PHNO_PVDED = "ERROR, Invalid Phone Number Entered !! \n Please Re-check it."
